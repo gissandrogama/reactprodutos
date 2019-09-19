7c265d218e27348772d32f72820ecb29cff0afd7
gissandrogama <gissandrogama@gmail.com>
2019-09-19 17:33:40 -0300

    remoção de link

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import "./styles.css"
 
 const Header = () => (
-    <header id="main-header">PRODUTOS</header>    
+    <header id="main-header">
+    <Link to={}>PRODUTOS</Link>
+    </header>    
 )
 
 export default Header
